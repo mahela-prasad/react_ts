59caf127ec1c7103f7389477c0249d09b3fdaf3f
Mahela Prasad <84281002+mahela-prasad@users.noreply.github.com>
2026-01-05 22:26:26 +0000

    sharing state

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,14 @@
 import { useState } from "react";
-import Button from "./components/Button";
-import produce from "immer";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
-  // const [drink, setDrink] = useState({
-  //   title: "Americano",
-  //   price: 5,
-  //   address: {
-  //     city: "London",
-  //     postCode: "NG2 5FQ",
-  //   },
-  // });
-
-  // const handleClickSetDrink = () => {
-  //   setDrink({
-  //     ...drink,
-  //     address: { ...drink.address, postCode: "W13 5QF" },
-  //   });
-  // };
-
-  // const [tags, setTags] = useState(["happy", "sad"]);
-
-  // function handleClickTags() {
-  //   // setTags([...tags, "mad"]);
-  //   // setTags(tags.filter((tag) => tag != "sad"));
-  //   setTags(tags.map((tag) => (tag === "sad" ? "moody" : tag)));
-  // }
-
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "bug 1", fixed: false },
-    { id: 2, title: "bug 2", fixed: false },
-  ]);
-
-  function handleClick() {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) bug.fixed = true;
-      })
-    );
-  }
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
   return (
     <div>
-      <li>
-        {bugs.map((bug) => (
-          <p key={bug.id}>
-            {bug.title} {bug.fixed ? "true" : "false"}
-          </p>
-        ))}
-      </li>
-      <button onClick={handleClick}>Click me</button>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 }
