2a9d78104298821194db62bcc38431c74945d95d
Mahela Prasad <84281002+mahela-prasad@users.noreply.github.com>
2026-01-05 21:10:27 +0000

    immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,58 @@
 import { useState } from "react";
-import ListGroup from "./components/ListGroup/ListGroup";
-import Like from "./components/Like";
+import Button from "./components/Button";
+import produce from "immer";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London"];
+  // const [drink, setDrink] = useState({
+  //   title: "Americano",
+  //   price: 5,
+  //   address: {
+  //     city: "London",
+  //     postCode: "NG2 5FQ",
+  //   },
+  // });
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleClickSetDrink = () => {
+  //   setDrink({
+  //     ...drink,
+  //     address: { ...drink.address, postCode: "W13 5QF" },
+  //   });
+  // };
+
+  // const [tags, setTags] = useState(["happy", "sad"]);
+
+  // function handleClickTags() {
+  //   // setTags([...tags, "mad"]);
+  //   // setTags(tags.filter((tag) => tag != "sad"));
+  //   setTags(tags.map((tag) => (tag === "sad" ? "moody" : tag)));
+  // }
+
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "bug 1", fixed: false },
+    { id: 2, title: "bug 2", fixed: false },
+  ]);
+
+  function handleClick() {
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+  }
 
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+      <li>
+        {bugs.map((bug) => (
+          <p key={bug.id}>
+            {bug.title} {bug.fixed ? "true" : "false"}
+          </p>
+        ))}
+      </li>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
