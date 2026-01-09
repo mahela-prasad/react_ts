92ce810b41b1dea33b8f092eca78ed00a351d3f6
Mahela Prasad <84281002+mahela-prasad@users.noreply.github.com>
2026-01-09 08:50:03 +0000

    react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,37 +1,49 @@
-import React, { useRef, type FormEvent } from "react";
+import React, { useRef, type FormEvent, useState } from "react";
+import { useForm, type FieldValue, type FieldValues } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const person = { name: "", age: 0 };
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current != null) {
-      console.log(nameRef.current.value + " submitted");
-      person.name = nameRef.current.value;
-    }
-    if (ageRef.current != null) {
-      console.log(ageRef.current.value + " submitted");
-      person.age = parseInt(ageRef.current.value);
-    }
-    console.log(person);
-  };
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">Name is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">Name is short</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
